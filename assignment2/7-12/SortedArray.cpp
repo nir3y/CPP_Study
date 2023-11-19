aca0d6a4524fc704fb073a8caf712d79fc81af2d
예린 정 <yerinlinlyn@gmail.com>
2023-11-19 21:23:11 +0900

    assignment 7-12 추가

---
+++ assignment2/7-12/SortedArray.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class SortedArray {
+	int size; // 현재 배열의 크기
+	int* p; // 정수 배열에 대한 포인터
+	void sort(); // 정수 배열을 오름차순으로 정렬
+public:
+	SortedArray(); // p는 NULL로 size는 0으로 초기화
+	SortedArray(SortedArray& src); // 복사 생성자
+	SortedArray(int p[], int size); // 생성자. 정수 배열과 크기를 전달받음
+	~SortedArray(); // 소멸자
+	SortedArray operator + (SortedArray& op2);
+	SortedArray& operator = (const SortedArray& op2); // 현재 배열에 op2 배열을 복사
+	void show(); // 배열의 원소 출력
+};
+
+int main()
+{
+	int n[] = { 2, 20, 6 };
+	int m[] = { 10, 7, 8, 30 };
+	SortedArray a(n, 3), b(m, 4), c;
+
+	c = a + b; // +, = 연산자 작성 필요
+	// + 연산자가 SortedArray 객체를 리턴하므로 복사 생성자 필요
+
+	a.show();
+	b.show();
+	c.show();
+
+	system("pause");
+	return 0;
+}
